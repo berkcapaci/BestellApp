807f16c4230736b0df931c76d902cbdc8f383b8f
BerkCapaci <berkcapaci@hotmail.com.tr>
2026-06-18 10:23:38 +0200

    data.js created, fonts and pizza images also added

---
+++ script/data.js
@@ -0,0 +1,130 @@
+const pizzas = [
+    {
+        id: 1,
+        name: "Salami",
+        description: "Tomatensauce, Mozzarella und Salami",
+        price: 11.90,
+        image: "salami.jpg",
+        vegetarian: false
+    },
+    {
+        id: 2,
+        name: "Prosciutto",
+        description: "Tomatensauce, Mozzarella und Kochschinken",
+        price: 12.90,
+        image: "prosciutto.jpg",
+        vegetarian: false
+    },
+    {
+        id: 3,
+        name: "Tonno",
+        description: "Tomatensauce, Mozzarella, Thunfisch und Zwiebeln",
+        price: 12.50,
+        image: "tonno.jpg",
+        vegetarian: false
+    },
+    {
+        id: 4,
+        name: "Hawaii",
+        description: "Tomatensauce, Mozzarella, Schinken und Ananas",
+        price: 12.90,
+        image: "hawaii.jpg",
+        vegetarian: false
+    },
+    {
+        id: 5,
+        name: "Diavola",
+        description: "Tomatensauce, Mozzarella und scharfe Salami",
+        price: 13.50,
+        image: "diavola.jpg",
+        vegetarian: false
+    },
+    {
+        id: 6,
+        name: "BBQ Chicken",
+        description: "BBQ-Sauce, Mozzarella und Hähnchenbrust",
+        price: 14.50,
+        image: "bbq_chicken.jpg",
+        vegetarian: false
+    },
+    {
+        id: 7,
+        name: "Sucuk",
+        description: "Tomatensauce, Mozzarella und Sucuk",
+        price: 13.90,
+        image: "sucuk.jpg",
+        vegetarian: false
+    },
+    {
+        id: 8,
+        name: "Quattro Stagioni",
+        description: "Tomatensauce, Mozzarella, Schinken, Pilze und Artischocken",
+        price: 14.90,
+        image: "quattro_stagioni.jpg",
+        vegetarian: false
+    },
+    {
+        id: 9,
+        name: "Margherita",
+        description: "Tomatensauce und Mozzarella",
+        price: 9.90,
+        image: "margherita.jpg",
+        vegetarian: true
+    },
+    {
+        id: 10,
+        name: "Funghi",
+        description: "Tomatensauce, Mozzarella und frische Champignons",
+        price: 10.90,
+        image: "funghi.jpg",
+        vegetarian: true
+    },
+    {
+        id: 11,
+        name: "Spinaci",
+        description: "Tomatensauce, Mozzarella und Blattspinat",
+        price: 11.50,
+        image: "spinaci.jpg",
+        vegetarian: true
+    },
+    {
+        id: 12,
+        name: "Caprese",
+        description: "Tomatensauce, Mozzarella, Tomaten und Basilikum",
+        price: 11.90,
+        image: "caprese.jpg",
+        vegetarian: true
+    },
+    {
+        id: 13,
+        name: "Vegetaria",
+        description: "Tomatensauce, Mozzarella und frisches Gemüse",
+        price: 12.90,
+        image: "vegetaria.jpg",
+        vegetarian: true
+    },
+    {
+        id: 14,
+        name: "Quattro Formaggi",
+        description: "Mozzarella, Gorgonzola, Parmesan und Gouda",
+        price: 13.90,
+        image: "quattro_formaggi.jpg",
+        vegetarian: true
+    },
+    {
+        id: 15,
+        name: "Rucola",
+        description: "Tomatensauce, Mozzarella, Rucola und Parmesan",
+        price: 12.90,
+        image: "rucola.jpg",
+        vegetarian: true
+    },
+    {
+        id: 16,
+        name: "Mediterranea",
+        description: "Tomatensauce, Mozzarella, Oliven, Paprika und Feta",
+        price: 13.50,
+        image: "mediterranea.jpg",
+        vegetarian: true
+    }
+];
