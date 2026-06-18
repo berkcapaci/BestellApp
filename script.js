2a74b9bebcb072cbca30e343f56acbf2848ded05
BerkCapaci <berkcapaci@hotmail.com.tr>
2026-06-18 20:24:48 +0200

    basket and co. lets call it a day.

--- script.js
+++ script.js
@@ -13,8 +13,50 @@ function renderPizzas(){
 }
 
 function renderBasket(){
+    if (basket.length === 0){
     basketContentRef.innerHTML = getBasketTemplate();
+    } else {
+        basketContentRef.innerHTML = '<h2>Your Basket</h2>';
+        let subtotal = 0;
+        
+        for (let i = 0; i < basket.length; i++) {
+            let currentBasketItem = basket[i];
+            let pizzaData = pizzas.find(
+                pizza => pizza.id ===currentBasketItem.id
+            );
+            subtotal += currentBasketItem.amount * pizzaData.price;
+            basketContentRef.innerHTML += getBasketItemTemplate(
+                currentBasketItem, pizzaData
+            );   
+        }
+        let deliveryCost = 5.00 ;
+        if (subtotal>=49.99){
+            deliveryCost = 0;
+        }
+        let totalPrice = subtotal + deliveryCost;
+        basketContentRef.innerHTML += getBasketSummaryTemplate(
+            subtotal, deliveryCost , totalPrice
+        );
+        basketContentRef.innerHTML += getBuyNowButtonTemplate();
+    }
 }
 
 renderPizzas();
 renderBasket();
+
+function orderNow(){
+    basket= [];
+    renderBasket();
+    showOrderDialog();
+}
+
+function showOrderDialog(){
+    document.getElementById('order-dialog').classList.remove('d-none');
+    setTimeout(() =>{
+        closeOrderDialog();
+    }, 3000);
+}
+
+function closeOrderDialog(){
+    document.getElementById('order-dialog').classList.add('d-none');
+}
--- script/templates.js
+++ script/templates.js
@@ -15,7 +15,9 @@ function getPizzaTemplate (currentPizza) {
 }
 
 function addToBasket(id){
-    let basketItem = basket.find(item => item.id ===id);
+
+    let basketItem = basket.find(item => item.id === id);
+
     if (basketItem) {
         basketItem.amount++;
     } else {
@@ -23,12 +25,58 @@ function addToBasket(id){
             id: id,
             amount : 1
         });
-    }    
+    }
+    renderBasket();    
 }
 
 function getBasketTemplate(){
     return `
         <h2> Your Basket </h2>    
         <p> Basket is empty </p>
+    `;
+}
+
+function getBasketItemTemplate (currentBasketItem, pizzaData){
+    let totalPrice = currentBasketItem.amount * pizzaData.price ;
+
+    return`
+        <div class="basket-item">
+            <span>${currentBasketItem.amount}x ${pizzaData.name}</span>
+            <span>${totalPrice.toFixed(2)} €</span>
+        </div>
+    `;
+}
+
+function getBasketSubtotalTemplate (subtotal){
+    return`
+        <div class="basket-item">
+            <span>Subtotal</span>
+            <span>${subtotal.toFixed(2)} €</span>
+        </div>    
+    `;
+}
+
+function getBasketSummaryTemplate (subtotal, deliveryCost, totalPrice){
+    return`
+        <div class="basket-item">
+            <span>Subtotal</span>
+            <span>${subtotal.toFixed(2)} €</span>
+        </div>   
+        <div class="basket-item">
+            <span>Delivery</span>
+            <span>${deliveryCost.toFixed(2)} €</span>
+        </div>   
+        <div class="basket-item">
+            <span>Total</span>
+            <span>${totalPrice.toFixed(2)} €</span>
+        </div>   
+    `;
+}
+
+function getBuyNowButtonTemplate(){
+    return `
+        <button onclick="orderNow()">
+        Buy Now
+        </button>
     `;
 }
